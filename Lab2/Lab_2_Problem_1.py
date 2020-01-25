a4a5a716b251fdf886705a4c3859b008738e9bbc
Ben <benjosephmartin@gmail.com>
2020-01-25 13:29:21 -0600

    Lab2 questions 1 done

--- Lab2/Lab_2_Problem_1.py
+++ Lab2/Lab_2_Problem_1.py
@@ -10,10 +10,44 @@ def evaluaterecurrence(myfunction, nvec):
         costs.append(funcDict[myfunction](n))
     return costs
 
+def genNvec(startingN, samples, rateOfChange):
+    toRet = []
+    n = startingN
+    for i in range(samples):
+        toRet.append(n)
+        n = n+rateOfChange
+    return toRet
+
+def genNvecPowersOf2(startingN, samples):
+    toRet = []
+    n = startingN
+    for i in range(samples):
+        toRet.append(n)
+        n = n *2
+    return toRet
+
 
 funcDict = {
-    'mergeSortFunc': functions.mergeSortFunc
+    'mergeSortFunc': functions.mergeSortFunc,
+    'f2': functions.hanoi,
+    'f3': functions.f3,
+    'f4': functions.f4
 }
 
-plt.plot(evaluaterecurrence('mergeSortFunc', np.linspace(1, 20)))
+nvec1 = genNvecPowersOf2(200, 100)
+nvec2 = genNvec(350, 20, 1)
+nvec3 = genNvec(350, 100, 1)
+nvec4 = genNvec(350, 100, 1)
+
+
+plt.plot(nvec1, evaluaterecurrence('mergeSortFunc', nvec1))
+plt.show()
+plt.figure()
+plt.plot(nvec2, evaluaterecurrence('f2', nvec2))
+plt.show()
+plt.figure()
+plt.plot(nvec3, evaluaterecurrence('f3', nvec3))
+plt.show()
+plt.figure()
+plt.plot(nvec4, evaluaterecurrence('f4', nvec4))
 plt.show()
--- Lab2/functions.py
+++ Lab2/functions.py
@@ -5,4 +5,25 @@ def mergeSortFunc(n):
     if(n <= 1):
         return 1
     else:
-        return 1+2*mergeSortFunc((n/2))+n
+        return 1+2*mergeSortFunc(math.floor(n/2))+n
+
+
+
+
+def hanoi(n):
+    if(n<=1):
+        return 1
+    else:
+        return 2*hanoi(n-1)+1
+
+def f3(n):
+    if (n <= 1):
+        return 1
+    else:
+        return 9*f3(n/3)+n
+
+def f4(n):
+    if (n <= 1):
+        return 1
+    else:
+        return 2*f4(n/4)+n*n
