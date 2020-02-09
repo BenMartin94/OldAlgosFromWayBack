96fdd2e512799bd78105d8aab80f0a73f09423da
Skylar <greensls@myumanitoba.ca>
2020-02-08 20:59:52 -0600

    Add bottomUpFibonacci, Remove redundant code

--- Lab3/Lab3Problem1.py
+++ Lab3/Lab3Problem1.py
@@ -19,28 +19,48 @@ def memoizedFibonacci(nth):
     else:
         nMinus1, nMinus2 =  memoizedFibonacci2(nth-1)
         return (nMinus1 + nMinus2)
+#memoizedFibonacci
 
 
 
-# memoized recursive Fibonacci method. Calculates n-1, and 
-# returns n-1 and n-2 to prevent redundant calculations
+# memoized recursive Fibonacci method. Calculates nth value, and 
+# returns n value and n-1 value to prevent redundant calculations
 def memoizedFibonacci2(nth):
     if (nth == 2):
-        return 1, 1
-    elif (nth == 3):
-        nMinus2 , nMinus3 = memoizedFibonacci2(2)
-        nMinus3 = 1
+        nMinus1 = 1
+        nMinus2 = 0
     else:
-        nMinus2, nMinus3 = memoizedFibonacci2(nth-1)
+        nMinus1, nMinus2 = memoizedFibonacci2(nth-1)
     
-    nMinus1 = nMinus2 + nMinus3
-    return nMinus1, nMinus2
+    nVal = nMinus1 + nMinus2
+    return nVal, nMinus1
+#memoizedFibonacci2
+
+
+
+# returns value of given nth fibonacci number
+def bottomupFibonacci(nth):
+    current = 1      # highest calculated fibonacci number
+    currentVal = 1   # value of highest calculated number
+    lastVal = 0      # value of the current-1 number
+
+    # calculation loop
+    while current < nth:
+        holder = currentVal
+        currentVal = currentVal + lastVal
+        lastVal = holder
+        current = current +1
+
+    return currentVal
+#bottomupFibonacci    
+
+
+
 
 
 
 for i in range(1,15):
-    print(i,  " : ",  recursiveFibonacci(i))
-    print(i,  " : ",  memoizedFibonacci(i))
+    print(i, ":", recursiveFibonacci(i), memoizedFibonacci(i), bottomupFibonacci(i))
     
 
 
