cc4ed5c1bed3445f42ed00ca477025e120ebe550
Skylar <greensls@myumanitoba.ca>
2020-02-08 20:34:58 -0600

    Add memoizedFibonacci method

--- Lab3/Lab3Problem1.py
+++ Lab3/Lab3Problem1.py
@@ -6,16 +6,42 @@ def recursiveFibonacci(nth):
 
     if (nth <=2 ):
         return 1
-
     else:
         return (recursiveFibonacci(nth-1) + recursiveFibonacci(nth-2))
 #recursiveFibonacci
 
 
 
+# memoized recursive Fibonacci method wrapper method
+def memoizedFibonacci(nth):
+    if (nth <=2):
+        return 1
+    else:
+        nMinus1, nMinus2 =  memoizedFibonacci2(nth-1)
+        return (nMinus1 + nMinus2)
+
+
+
+# memoized recursive Fibonacci method. Calculates n-1, and 
+# returns n-1 and n-2 to prevent redundant calculations
+def memoizedFibonacci2(nth):
+    if (nth == 2):
+        return 1, 1
+    elif (nth == 3):
+        nMinus2 , nMinus3 = memoizedFibonacci2(2)
+        nMinus3 = 1
+    else:
+        nMinus2, nMinus3 = memoizedFibonacci2(nth-1)
+    
+    nMinus1 = nMinus2 + nMinus3
+    return nMinus1, nMinus2
+
+
+
 for i in range(1,15):
     print(i,  " : ",  recursiveFibonacci(i))
-
+    print(i,  " : ",  memoizedFibonacci(i))
+    
 
 
 print("\n Program finished sucessfully")
