32ba9b2c12a048811c537c3ad9ff1911a96e354d
Ben <benjosephmartin@gmail.com>
2020-02-11 16:23:29 -0600

    Memoized done

--- Lab3/Lab3Problem2.py
+++ Lab3/Lab3Problem2.py
@@ -19,23 +19,28 @@ def bottomUp(x, y):
         for j in range(len(y)):
             if(i==0 or j == 0):
                 maxSoFar[i, j] = 0
-            elif(x[i-1] == y[j-1]):
+            elif(x[i-1] == y[j-1]):##cant be 0 because that was handled above
                 maxSoFar[i,j] = maxSoFar[i-1, j-1]+1
             else:
                 maxSoFar[i,j] = max(maxSoFar[i, j-1], maxSoFar[i-1, j])
     return np.max(maxSoFar)
 
-    '''
-    k = min(len(x), len(y))
-    maxSoFar = np.zeros(k)
-    i = 0
-    j = 0
-    while i<len(i):
-        while j<len(y):
-            
-    '''
+def recursiveMemoLCS(x,y, memo):
+    if(len(x) == 0 or len(y) == 0):
+        memo[len(x)-1, len(y)-1] = 0
+    xF = x[len(x) - 1]
+    yF = y[len(y) - 1]
+    if(xF == yF):
+        memo[len(x)-1, len(y)-1] = (recursiveLCS(x[0:len(x)-1], y[0:len(y)-1]))+1
+        return(memo[len(x)-1, len(y)-1])
+    else:
+        memo[len(x)-2, len(y)-1] = recursiveLCS(x[0:len(x)-1], y)
+        memo[len(x)-1, len(y)-2] = recursiveLCS(x, y[0:len(y)-1])
+        return max(memo[len(x)-1, len(y)-2], memo[len(x)-2, len(y)-1])
 x = [3,6,4, 2, 1, 5, 6, 8]
 y = [1,3,5,4]
 
 print(recursiveLCS(x, y))
 print(bottomUp(x, y))
+memoy = np.zeros((len(x), len(y)))
+print(recursiveMemoLCS(x, y, memoy))
