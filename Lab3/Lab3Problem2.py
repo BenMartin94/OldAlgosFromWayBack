108b62c9f4d2d6c428ff5281ea3d1acd40ec26de
Skylar <greensls@myumanitoba.ca>
2020-02-18 09:55:36 -0600

    add plots

--- Lab3/Lab3Problem2.py
+++ Lab3/Lab3Problem2.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 
 
 
@@ -148,45 +148,135 @@ def getSubArray(arrayx, xBin):
 #getSubArray
 
 
+
+
+
+# function to generate linear vector of values from given starting number, with given interval
+def genNvec(startingN, samples, rateOfChange):
+    toRet = []
+    n = startingN
+    for i in range(samples):
+        toRet.append(n)
+        n = n+rateOfChange
+    return toRet
+#genNvec
+
+
+
+
+
+
 def main():
-    #generate random integer arrays (Min_Val, Max_Val, Length)
-    x = np.random.randint(0, 5, 10) 
-    y = np.random.randint(0, 5, 9) 
-    print("array x: ", x)
-    print("array y: ", y, "\n")
+
+    n = 12
+
+    #generate vectors on n values (starting n, how many n, step)
+    nVec1 = genNvec(1, n, 1)
+    nVec2 = genNvec(1, n, 1)
 
 
-    startTime = time.time()
-    recuresiveVal = recursiveLCS(x, y)
-    recursiveTime = time.time() - startTime
+    recursiveTimes = []
+    for length in nVec1:
+        x = np.random.randint(0, 9, length) 
+        y = np.random.randint(0, 9, length) 
+
+        startTime = time.time()
+        recuresiveVal = recursiveLCS(x, y)
+        recursiveTimes.append(time.time() - startTime)
 
 
-    startTime = time.time()
-    memoVal = memoizedLCS(x, y)
-    memoTime = time.time() - startTime
+    memoizedTimes = []
+    for length in nVec2:
+        x = np.random.randint(0, 9, length) 
+        y = np.random.randint(0, 9, length) 
+
+        startTime = time.time()
+        memoizedVal = memoizedLCS(x, y)
+        memoizedTimes.append(time.time() - startTime)
+
+    exp = []
+    for val in nVec1:
+        exp.append((val**(val/3))/(100000))
 
 
+
+    # plot results of recursive method
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_yscale('log')
+    plt.plot(nVec1, recursiveTimes)
+    plt.plot(nVec1, exp)
+    plt.xlabel("x, y array length")
+    plt.ylabel("time (s)")
+    plt.title("Recursive LCS Method Calculation Times")
+    plt.legend(['Recursive Method Times', '(n^n/3)x10^-6'])
+    plt.show()
+    
+    # plot results of rmemoized method
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.plot(nVec1, memoizedTimes)
+    ax.set_yscale('log')
+    plt.plot(nVec1, exp)
+    plt.xlabel("x, y array length")
+    plt.ylabel("time (s)")
+    plt.title("Memoized LCS Method Calculation Times")
+    plt.legend(['Memoized Method Times', '(n^n/3)x10^-6'])
+    plt.show()
+
+    x = np.random.randint(0, 9, 5)
+    y = np.random.randint(0, 9, 5)
     startTime = time.time()
     bruteVal = bruteForceLCS(x, y)
     bruteTime = time.time() - startTime
 
 
 
-    startTime = time.time()
-    bottomUpVal = bottomUp(x, y)
-    bottomUpTime = time.time()-startTime
-
 
     # output results
-    print("Method\t\t LCS\t time (s)")
-    print("-----------------------------------------")
-    print("Recursive:\t", recuresiveVal, "\t", recursiveTime)
-    print("Memoized: \t", memoVal, "\t", memoTime)
     print("Brute Force:\t", bruteVal[0], "\t", bruteTime)
-    #print("Bottom Up: \t", bottomUpVal, "\t", bottomUpTime) # DOESN'T WORK
-
-
     print("\nLCS:\t", bruteVal[1])
+
+
+    # # 3d plot 
+
+    # nVec1 = genNvec(0, 10, 1)
+
+    # mVec1 = genNvec(0, 20, 1)
+
+    # times = np.zeros((len(nVec1), len(mVec1)))
+    # nVec = []
+    # mVec = []
+
+    # for n in nVec1:
+    #     nVec.append([])
+    #     mVec.append([])
+    #     for m in mVec1:
+    #             x = np.random.randint(0, 5, m) 
+    #             y = np.random.randint(0, 5, n) 
+    #             startTime = time.time()
+    #             recuresiveVal = recursiveLCS(x, y)
+    #             recursiveTime = time.time() - startTime
+    #             times[n][m] = recursiveTime
+    #             nVec[n].append(n)
+    #             mVec[n].append(m)
+
+
+    # # plot results of bottom up method
+
+
+    # fig = plt.figure()
+    # fig.patch.set_facecolor('white')
+    # recursivePlot = fig.add_subplot(111, projection='3d')
+    # recursivePlot.plot_surface(nVec, mVec, times, cmap='viridis', edgecolor='none')
+    # recursivePlot.set_xlabel("n")
+    # recursivePlot.set_ylabel("m")
+    # recursivePlot.set_zlabel("time (s)")
+    # recursivePlot.set_title("Recursive LCS Method Calculation Times")
+    # #recursivePlot.legend(['Recursive Method Times'])
+    # plt.show()
+    
+
 
     return("Program Ended Successfully")
 #main
