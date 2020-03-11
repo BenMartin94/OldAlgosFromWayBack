df518673e50e14c2bbbf42bfe1ad186f0bb1a902
Ben <benjosephmartin@gmail.com>
2020-03-11 13:59:22 -0500

    Data collection

--- Lab4/LUSolve.m
+++ Lab4/LUSolve.m
@@ -3,18 +3,11 @@ function x = LUSolve(A, b)
   for i = 1:length(A)
     d(i,1) = b(i,1);
     d(i,1) = d(i,1) - A(i,1:i-1)*d(1:i-1,1);
-    #for j = 1:i-1;
-     # d(i,1) = d(i,1)-A(i,j)*d(j);
-    #endfor
-    
   endfor
   x = zeros(length(A), 1);
   for i = length(A):-1:1;
     x(i,1)=d(i,1);
-    x(i,1) = x(i,1)-A(i,i+1:length(A))*x(i+1:length(x),1)
-    #for j = length(A):-1:i+1
-     # x(i,1) = x(i,1)-A(i,j)*x(j,1);
-    #endfor
+    x(i,1) = x(i,1)-A(i,i+1:length(A))*x(i+1:length(x),1);
     x(i,1) = x(i,1)/A(i,i);
   endfor
 endfunction
