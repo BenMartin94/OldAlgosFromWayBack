60d2969546b1afad5c8fab17ca9df908f5318489
Ben <Benjosephmartin@gmail.com>
2021-09-22 17:10:54 -0500

    Sums up properly now

--- Lab1/program_3a.cpp
+++ Lab1/program_3a.cpp
@@ -38,7 +38,12 @@ int main(int argc, char** argv)
         srand(clock());										//Seed RNG
 
         vector<double> full_list(nvalues_per_proc*nproc);
-        
+        generate_randoms(&full_list, nvalues_per_proc*nproc);
+        for(int irank = 1; irank<nproc; irank++){
+            vector<double> toSend = subvec(full_list, (irank) * nvalues_per_proc, (irank+1) * nvalues_per_proc);
+            MPI_Send(&toSend[0], nvalues_per_proc, MPI_DOUBLE, irank, 9, MPI_COMM_WORLD);
+        }
+
         //Generate and distribute the full_list of values to all other processors
         //REMEMBER: the other processors don't know what nvalues_per_proc is
 
@@ -47,27 +52,82 @@ int main(int argc, char** argv)
         //Receive all partial sums from the other processors
         //Produce the total sum from the partial sums locally
         //Compare to the sum of MPI_Reduce
-        double partial_sum;
-        for(irank = 1; irank < nproc; irank++)
+        double partial_sum = 0;
+        double myPartialSum=0;
+        // now compute the masters part since this is a fair world :)
+        for(int i = 0; i<nvalues_per_proc;i++){
+            partial_sum += full_list[i];
+        }
+        myPartialSum = partial_sum;
+        for(int irank = 0; irank < nproc; irank++)
         {
+            if(irank != 0)
+                MPI_Recv(&partial_sum, 1, MPI_DOUBLE, irank, 9, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
             std::cout << "Partial Sum on Processor " << irank << ": " << partial_sum << std::endl;
+            partial_sum = 0;
         }
 
+        double total_sum_reduce;
+        MPI_Reduce(&myPartialSum, &total_sum_reduce, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
         std::cout << "Total Sum on all Processors (MPI_Reduce): " << total_sum_reduce << std::endl;
+
+
+        double total_sum_manual = 0;
+        for(int i = 0;i<full_list.size(); i++){
+            //cout << full_list[i] << endl;
+            total_sum_manual += full_list[i];
+        }
+
 		std::cout << "Total Sum on all Processors (Manual sum): " << total_sum_manual << std::endl;
 
-        std::cout << "Total time to compute: " << time2-time1 << " seconds" << std::endl;
+        //std::cout << "Total time to compute: " << time2-time1 << " seconds" << std::endl;
     }
     else
     {
+        MPI_Status status;
+		MPI_Probe(0,9,MPI_COMM_WORLD,&status);
+        int numValuesToSum;
+
+        MPI_Get_count(&status, MPI_DOUBLE, &numValuesToSum);
+
+        vector<double> myPortion(numValuesToSum);
+
+        MPI_Recv(&myPortion[0], numValuesToSum, MPI_DOUBLE, 0, 9, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
+
+        double sum = 0;
+        for(int i = 0;i<myPortion.size(); i++){
+            sum += myPortion[i];
+        }
+
+        MPI_Send(&sum, 1, MPI_DOUBLE, 0, 9, MPI_COMM_WORLD);
+        MPI_Reduce(&sum, NULL, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
         //Collect the subvector from rank 0
         //Calculate the local sum
         //Use MPI_REDUCE to contribute to the total sum on rank 0
         //Send the partial sum to rank 0 for displaying and verifying
+        
     }
 
     MPI_Finalize();
 
     return 0;
 
+}
+
+void generate_randoms(vector<double> *list, double num_rands){
+    for(int i = 0;i<num_rands; i++){
+        list->at(i) = rand();
+    }
+}
+
+vector<double> subvec(vector<double> list, int slice_start, int slice_end){
+    vector<double>::iterator first = list.begin() + slice_start;
+    vector<double>::iterator last = list.begin() + slice_end;
+    vector<double> subvector(first, last);
+    return subvector;
 }
